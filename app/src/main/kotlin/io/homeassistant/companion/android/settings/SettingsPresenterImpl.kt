5ec74c56eec81e1a96a80bc0cbe3744c5fa05cdc
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-05-21 10:51:52 +0200

    Replace Build.Version.SDK_INT usage

--- app/src/main/kotlin/io/homeassistant/companion/android/settings/SettingsPresenterImpl.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/settings/SettingsPresenterImpl.kt
@@ -13,6 +13,7 @@ import io.homeassistant.companion.android.common.data.integration.impl.entities.
 import io.homeassistant.companion.android.common.data.prefs.NightModeTheme
 import io.homeassistant.companion.android.common.data.prefs.PrefsRepository
 import io.homeassistant.companion.android.common.data.servers.ServerManager
+import io.homeassistant.companion.android.common.util.sdkVersion
 import io.homeassistant.companion.android.database.server.Server
 import io.homeassistant.companion.android.database.settings.SettingsDao
 import io.homeassistant.companion.android.settings.assist.DefaultAssistantManager
@@ -198,7 +199,7 @@ class SettingsPresenterImpl @Inject constructor(
         }
 
         // Notifications
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&
+        if (sdkVersion.isAtLeast(Build.VERSION_CODES.O) &&
             !NotificationManagerCompat.from(context).areNotificationsEnabled()
         ) {
             suggestions += SettingsHomeSuggestion(
